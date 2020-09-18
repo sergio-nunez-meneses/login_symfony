31a24c0376de0b7d9596aa9ed409291c67d50f1a
sergio núñez meneses <63654322+sergio-nunez-meneses@users.noreply.github.com>
2020-09-18 10:01:04 +0200

    added twig template to HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,14 +1,38 @@
 <?php
 
 namespace App\Controller;
-use Symfony\Component\HttpFoundation\Response;
 
-class HomeController
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Users;
+use App\Repository\UsersRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+
+class HomeController extends AbstractController
 {
+
+  /**
+   * @var Environment
+   */
+  private $twig;
+
+  public function __construct(Environment $twig)
+  {
+    $this->twig = $twig;
+  }
 
   public function index(): Response
   {
+    $repository = $this->getDoctrine()->getRepository(Users::class);
+    $users = $repository->findAll();
+    var_dump($users);
+
     $id = 1;
-    return new Response('<h1>Howdy, <em>stranger!</em><h1>');
+    $user = $repository->find($id);
+    var_dump($user);
+
+
+    return new Response($this->twig->render('pages/home.html.twig'));
   }
 }
